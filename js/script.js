274b45d25bf6c4113f7da16ade9496788408b31d
Debolina903 <debolinab121@gmail.com>
2025-07-03 19:20:25 +0530

    testimonial section updated

---
+++ js/script.js
@@ -0,0 +1,98 @@
+const toggle = document.getElementById("burger");
+const navLeft = document.querySelector(".nav-left");
+const navRight = document.querySelector(".nav-right");
+
+toggle.addEventListener("click", () => {
+  navLeft.classList.toggle("active");
+  navRight.classList.toggle("active");
+});
+
+
+// PROPERTY SECTION JS 
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".propertySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: false,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+      1280: {
+        slidesPerView: 4,
+      },
+    },
+  });
+});
+// TESTIMONIAL SECTION 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const testimonialSwiper = new Swiper(".testimonialSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      }
+    }
+  });
+});
+// TRUSTED SWIPER AUTO SCROLL 
+const swiper = new Swiper('.trusted-swiper', {
+      loop: true,
+      autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+      },
+      speed: 3000,
+      grabCursor: true,
+      freeMode: true,
+      breakpoints: {
+        0: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 45,
+          gap:20,
+        },
+      }
+    });
